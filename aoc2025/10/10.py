3cba432d2711526ce2bedf96cd65d02217ab2b60
Cameron Fraser <cfraser497@gmail.com>
2025-12-29 00:50:17 +0000

    day 10 part 1

--- aoc2025/10/10.py
+++ aoc2025/10/10.py
@@ -1,13 +1,103 @@
 import argparse
+from collections import deque
+
+# idea 1: BFS
+
+ON = '#'
+PART1 = False
+
+def parse_machines(file_input):
+    machines = []
+    
+    for line in file_input:
+        indicators = line.split(']')[0][1:]
+        indicators = [i == ON for i in indicators]
+        
+        buttons = line.split(' ')[1:-1]
+        buttons = [list(map(int, b[1:-1].split(','))) for b in buttons]
+        buttons.sort(key=lambda bs: len(bs), reverse=True)
+
+        joltage_reqs = line.split('{')[1][:-1]
+        joltage_reqs = [int(j) for j in joltage_reqs.split(',')]
+
+        machine = (indicators, buttons, joltage_reqs)
+        machines.append(machine)
+    return machines
 
 def solve(file_input):
-    print(file_input)
-    return 0
+    machines = parse_machines(file_input)
+    total_presses = 0
+
+    if PART1:
+        for machine in machines:
+            print("MACHINE", machine)
+            indicators, buttons, joltages = machine
+            initial_state = [False for _ in indicators]
+            if initial_state == indicators:
+                continue
+            
+            q = deque([(initial_state, [])])
+
+            while q:
+                state, presses = q.popleft()
+
+                for b in buttons:
+                    new_state = state.copy()
+                    # switch the lights
+                    for light in b:
+                        
+                        new_state[light] = not state[light]
+
+                    if new_state == indicators:
+                        print(presses + [b], len(presses) + 1)
+                        total_presses += (len(presses) + 1)
+                        q = None
+                        break
+
+                    q.append((new_state, presses + [b]))
+
+
+    else:
+        # too slow T_T
+        for machine in machines:
+            print("MACHINE", machine)
+            _, buttons, joltages = machine
+            initial_state = [0 for _ in joltages]
+            if initial_state == joltages:
+                continue
+            
+            q = deque([(initial_state, [])])
+            seen = {tuple(initial_state)}
+
+            while q:
+                state, presses = q.popleft()
+
+                for b in buttons:
+                    new_state = state.copy()
+                    # switch the lights
+                    for light in b:
+                        new_state[light] += 1
+
+                    new_presses = presses + [b]
+                    if new_state == joltages:
+                        print(new_presses, len(new_presses))
+                        total_presses += (len(new_presses))
+                        q = None
+                        break
+
+                    # only append valid states
+                    valid = [j <= joltages[i] for i, j in enumerate(new_state)]
+                    if all(valid) and tuple(new_state) not in seen:
+                        # print(new_state, len(new_presses))
+                        seen.add(tuple(new_state))
+                        q.append((new_state, new_presses))
+
+    return total_presses
     
 
 def readFile(file):
     with open(f"{file}.txt", 'r') as f:
-        file_contents = f.read().split()
+        file_contents = f.read().split('\n')
 
     return file_contents
 
