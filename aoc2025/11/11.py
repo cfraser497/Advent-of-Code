7b64eb558f289964500c46e4f8bf87b5eab88042
Cameron Fraser <cfraser497@gmail.com>
2025-12-29 11:24:05 +0000

    day 11

--- aoc2025/11/11.py
+++ aoc2025/11/11.py
@@ -1,13 +1,53 @@
 import argparse
+from collections import defaultdict
+
+OUT = 'out'
+YOU = 'you'
+SVR = 'svr'
+FFT = 'fft'
+DAC = 'dac'
 
 def solve(file_input):
+    graph = {}
+    for device in file_input:
+        device_name = device.split(":")[0]
+        targets = device.split(":")[1].strip().split(" ")
+
+        graph[device_name] = targets
     
-    return 0
-    
+
+    cache = defaultdict(dict)
+
+    def dfs(node, path):
+        def dict_key():
+            dac = DAC if DAC in path else ''
+            fft = FFT if FFT in path else ''
+            return ''.join([dac, fft])
+
+        if node in cache and dict_key() in cache[node]:
+            return cache[node][dict_key()]
+        
+        if node == OUT:
+            # uncomment for part 1
+            # return 1
+            return 1 if DAC in path and FFT in path else 0
+        
+        paths = 0
+        for target in graph[node]:
+            paths += dfs(target, path + [target])    
+        
+
+        cache[node][dict_key()] = paths
+        return paths
+
+    part1 = dfs(YOU, [YOU])
+    part2 = dfs(SVR, [SVR])
+    return (part1, part2)
+
 
 def readFile(file):
     with open(f"{file}.txt", 'r') as f:
-        file_contents = f.read().split()
+        file_contents = f.read().split('\n')
 
     return file_contents
 
